c56a22415660ad3805d5b8a793f590d440210100
Krasnov <roman.krasnov@nure.ua>
2020-08-28 13:51:06 +0300

    DE-002 : fixed modal, updated code-style, updated mark-up

--- ModalChangeTask.js
+++ ModalChangeTask.js
@@ -1,33 +1,23 @@
-import {createInputGroup, createInputTask, isValidDate, isValidEnter, isValidTime, convertDate, convertDateReadable, markAsInvalid, sliceElementText} from "./Functions.js";
+import {createInputGroup, isValidDate, isValidEnter, convertDate, convertDateReadable, markAsInvalid, sliceElementText} from "./Functions.js";
 import {Task} from "./Task.js";
 import {convertForInputDate} from "./Functions.js";
 
 
 export class ModalChangeTask {
     selfId
-    toChangeTaskId
-    toChangeCreationDateId
-    toChangeExpirationDateId
-    toChangeCreationTimeId
-    toChangeExpirationTimeId
     hText
+    targetObject
 
     constructor(modalChangeData) {
         Object.assign(this, { ...modalChangeData });
+
         this.selfInputTaskId = `selfInputTaskId${Task.getUniqueId()}`;
         this.selfInputCreationDateId = `selfInputCreationDateId${Task.getUniqueId()}`;
         this.selfInputExpirationDateId = `selfInputExpirationDateId${Task.getUniqueId()}`;
-        this.selfInputTimeCreationId = `selfInputTimeCreationId${Task.getUniqueId()}`;
-        this.selfInputTimeExpirationId = `selfInputTimeExpirationId${Task.getUniqueId()}`;
         this.buttonSAVEId = `buttonSAVEId${Task.getUniqueId()}`;
         this.buttonCANCELId = `buttonCANCELId${Task.getUniqueId()}`;
 
-        // choose which modal to create : with time edit or not (depends on task)
-        if(document.getElementById(this.toChangeCreationTimeId) !== null) {
-            this.initializeAssembleFull()
-        } else {
-            this.initializeAssembleSimplified();
-        }
+        this.initializeAssembleFull()
     }
 
     initializeChangeModal() {
@@ -36,11 +26,21 @@ export class ModalChangeTask {
                 <div class="modal_content">
                     <h2>${this.hText}</h2>
                    
-                    ${createInputTask(this.selfInputTaskId, `Task : `)}
-                    ${createInputGroup(this.selfInputCreationDateId, 'Creation Date :')}
-                    ${createInputGroup(this.selfInputExpirationDateId, 'Expiration Date :')}
-                    ${createInputTask(this.selfInputTimeCreationId, 'Creation Time :')}
-                    ${createInputTask(this.selfInputTimeExpirationId, 'Expiration Time :')}
+                    ${createInputGroup({
+                        inputId : this.selfInputTaskId, 
+                        pText : `Task : `, 
+                        inputType : 'text'
+                    })}
+                    ${createInputGroup({
+                        inputId : this.selfInputCreationDateId, 
+                        pText : `Creation Date :`, 
+                        inputType : 'date'
+                    })}
+                    ${createInputGroup({
+                        inputId : this.selfInputExpirationDateId, 
+                        pText : 'Expiration Date :', 
+                        inputType : 'date'
+                    })}
                     
                     <a href="#" class="buttonModal" id="${this.buttonSAVEId}">SAVE</a>
                     <a href="#" class="buttonModal" id="${this.buttonCANCELId}">CANCEL</a>
@@ -48,108 +48,42 @@ export class ModalChangeTask {
             </div>
         `
     }
+
     initializeInputDefaultValues() {
-        document.getElementById(this.selfInputTaskId).value = sliceElementText(document.getElementById(this.toChangeTaskId).innerText);
-        document.getElementById(this.selfInputCreationDateId).value = convertForInputDate(sliceElementText(document.getElementById(this.toChangeCreationDateId).innerText));
-        document.getElementById(this.selfInputExpirationDateId).value = convertForInputDate(sliceElementText(document.getElementById(this.toChangeExpirationDateId).innerText));
-        document.getElementById(this.selfInputTimeCreationId).value = sliceElementText(document.getElementById(this.toChangeCreationTimeId).innerText);
-        document.getElementById(this.selfInputTimeExpirationId).value = sliceElementText(document.getElementById(this.toChangeExpirationTimeId).innerText);
+        document.getElementById(this.selfInputTaskId).value = this.targetObject.text;
+        document.getElementById(this.selfInputCreationDateId).value = convertForInputDate(this.targetObject.creationDate);
+        document.getElementById(this.selfInputExpirationDateId).value = convertForInputDate(this.targetObject.expirationDate);
     }
+
     static initializeChangeHandlers(modal) {
         document.getElementById(modal.buttonSAVEId).addEventListener('click', function OKClicked() {
 
             const selfInputTaskElem = document.getElementById(modal.selfInputTaskId);
             const selfInputCreationDateElem = document.getElementById(modal.selfInputCreationDateId);
             const selfInputExpirationDateElem = document.getElementById(modal.selfInputExpirationDateId);
-            const selfInputTimeCreationElem = document.getElementById(modal.selfInputTimeCreationId);
-            const selfInputTimeExpirationElem = document.getElementById(modal.selfInputTimeExpirationId);
 
             const changedTaskText = document.getElementById(modal.selfInputTaskId).value;
             const changedTaskCreationDate = document.getElementById(modal.selfInputCreationDateId).value;
             const changedTaskExpirationDate = document.getElementById(modal.selfInputExpirationDateId).value;
-            const changedTaskCreationTime = document.getElementById(modal.selfInputTimeCreationId).value;
-            const changedTaskExpirationTime = document.getElementById(modal.selfInputTimeExpirationId).value;
 
-            const toChangeTaskElem = document.getElementById(modal.toChangeTaskId);
-            const toChangeCreationDateElem = document.getElementById(modal.toChangeCreationDateId);
-            const toChangeExpirationDateElem = document.getElementById(modal.toChangeExpirationDateId);
-            const toChangeCreationTimeElem = document.getElementById(modal.toChangeCreationTimeId);
-            const toChangeExpirationTimeElem = document.getElementById(modal.toChangeExpirationTimeId);
+            const toChangeTaskElem = document.getElementById(modal.targetObject.taskTextId);
+            const toChangeCreationDateElem = document.getElementById(modal.targetObject.creationDateId);
+            const toChangeExpirationDateElem = document.getElementById(modal.targetObject.expirationDateId);
 
-            let allChecked = (isValidEnter(changedTaskText) && isValidDate(convertDate(changedTaskCreationDate), convertDate(changedTaskExpirationDate)) &&
-                              isValidTime(changedTaskCreationTime) && isValidTime(changedTaskExpirationTime));
+            let allChecked = (isValidEnter(changedTaskText) && isValidDate(convertDate(changedTaskCreationDate), convertDate(changedTaskExpirationDate)));
 
             if(allChecked) {
                 toChangeTaskElem.innerText = `Task: ${changedTaskText}`;
                 toChangeCreationDateElem.innerText = `Creation Date: ${convertDateReadable(changedTaskCreationDate)}`;
                 toChangeExpirationDateElem.innerText = `Expiration Date: ${convertDateReadable(changedTaskExpirationDate)}`;
-                toChangeCreationTimeElem.innerText = `Creation Time: ${changedTaskCreationTime}`;
-                toChangeExpirationTimeElem.innerText = `Expiration Time: ${changedTaskExpirationTime}`;
 
-                document.getElementById(modal.selfId).style.display = "none";
-            } else {
-                if(!isValidEnter(changedTaskText)){
-                    markAsInvalid(selfInputTaskElem);
-                }
-                if(!isValidDate(convertDate(changedTaskCreationDate), convertDate(changedTaskExpirationDate))) {
-                    markAsInvalid(selfInputCreationDateElem);
-                    markAsInvalid(selfInputExpirationDateElem);
-                }
-                if(!isValidTime(changedTaskCreationTime)){
-                    markAsInvalid(selfInputTimeCreationElem);
-                }
-                if(!isValidTime(changedTaskExpirationTime)){
-                    markAsInvalid(selfInputTimeExpirationElem);
-                }
-            }
-        });
+                const changedObject = {
+                    ...modal.targetObject,
+                    text : changedTaskText,
+                    creationDate : changedTaskCreationDate,
+                    expirationDate : changedTaskExpirationDate};
 
-        document.getElementById(modal.buttonCANCELId).addEventListener('click', function cancelClicked() {
-            document.getElementById(modal.selfId).style.display = "none";
-        });
-    }
-
-    initializeChangeModalSimplified() {
-        document.getElementById('forChangeModal').innerHTML = `
-            <div id="${this.selfId}" class="modal">
-                <div class="modal_content">
-                    <h2>${this.hText}</h2>
-                   
-                    ${createInputTask(this.selfInputTaskId, `Task : `)}
-                    ${createInputGroup(this.selfInputCreationDateId, 'Creation Date :')}
-                    ${createInputGroup(this.selfInputExpirationDateId, 'Expiration Date :')}
-                    
-                    <a href="#" class="buttonModal" id="${this.buttonSAVEId}">SAVE</a>
-                    <a href="#" class="buttonModal" id="${this.buttonCANCELId}">CANCEL</a>
-                </div>
-            </div>
-        `
-    }
-    initializeInputDefaultValuesSimplified() {
-        document.getElementById(this.selfInputTaskId).value = sliceElementText(document.getElementById(this.toChangeTaskId).innerText);
-        document.getElementById(this.selfInputCreationDateId).value = convertForInputDate(sliceElementText(document.getElementById(this.toChangeCreationDateId).innerText));
-        document.getElementById(this.selfInputExpirationDateId).value = convertForInputDate(sliceElementText(document.getElementById(this.toChangeExpirationDateId).innerText));
-    }
-    static initializeChangeHandlersSimplified(modal) {
-        document.getElementById(modal.buttonSAVEId).addEventListener('click', function OKClicked() {
-
-            const selfInputTaskElem = document.getElementById(modal.selfInputTaskId);
-            const selfInputCreationDateElem = document.getElementById(modal.selfInputCreationDateId);
-            const selfInputExpirationDateElem = document.getElementById(modal.selfInputExpirationDateId);
-
-            const changedTaskText = document.getElementById(modal.selfInputTaskId).value;
-            const changedTaskCreationDate = document.getElementById(modal.selfInputCreationDateId).value;
-            const changedTaskExpirationDate = document.getElementById(modal.selfInputExpirationDateId).value;
-
-            const toChangeTaskElem = document.getElementById(modal.toChangeTaskId);
-            const toChangeCreationDateElem = document.getElementById(modal.toChangeCreationDateId);
-            const toChangeExpirationDateElem = document.getElementById(modal.toChangeExpirationDateId);
-
-
-            if(isValidEnter(changedTaskText) && isValidDate(convertDate(changedTaskCreationDate), convertDate(changedTaskExpirationDate))) {
-                toChangeTaskElem.innerText = `Task: ${changedTaskText}`;
-                toChangeCreationDateElem.innerText = `Creation Date: ${convertDateReadable(changedTaskCreationDate)}`;
-                toChangeExpirationDateElem.innerText = `Expiration Date: ${convertDateReadable(changedTaskExpirationDate)}`;
+                Task.replaceTaskWithChanged(changedObject);
 
                 document.getElementById(modal.selfId).style.display = "none";
             } else {
@@ -172,12 +106,6 @@ export class ModalChangeTask {
         this.initializeChangeModal();
         ModalChangeTask.initializeChangeHandlers(this);
         this.initializeInputDefaultValues()
-        this.visible();
-    }
-    initializeAssembleSimplified() {
-        this.initializeChangeModalSimplified();
-        ModalChangeTask.initializeChangeHandlersSimplified(this);
-        this.initializeInputDefaultValuesSimplified()
         this.visible();
     }
 
--- Task.js
+++ Task.js
@@ -3,18 +3,14 @@ import {tasksObj} from "./Variables.js";
 export class Task {
     text
     creationDate
-    creationTime
     expirationDate
-    expirationTime
 
     constructor(taskData) {
         Object.assign(this, { ...taskData });
 
         this.taskTextId = `taskTextId${Task.getUniqueId()}`;
         this.creationDateId = `creationDateId${Task.getUniqueId()}`;
-        this.creationTimeId = `creationTimeId${Task.getUniqueId()}`;
         this.expirationDateId = `expirationDateId${Task.getUniqueId()}`;
-        this.expirationTimeId = `expirationTimeId${Task.getUniqueId()}`;
         this.pencilId = `pencil${Task.getUniqueId()}`
         this.checkBoxId = `checkbox${Task.getUniqueId()}`;
         this.divId = `divId${Task.getUniqueId()}`;
@@ -27,43 +23,22 @@ export class Task {
 
     getInnerHtml() {
         return `
-            <div style="display: flex" id="${this.mainId}">
-                <div style="width: 45%" id="${this.divId}">
-                    <p>
-                        <strong id=${this.taskTextId}>Task: ${this.text}</strong> <br> 
-                        <strong id=${this.creationDateId}>Creation Date: ${this.creationDate}</strong> <br>
-                        <strong id=${this.creationTimeId}>Creation Time: ${this.creationTime}</strong> <br>
-                        <strong id=${this.expirationDateId}>Expiration Date: ${this.expirationDate}</strong> <br>
-                        <strong id=${this.expirationTimeId}>Expiration Time: ${this.expirationTime}</strong> <br> 
-                    </p>
-                </div>
-                <div style="width: 45%;">
-                    <div class="pencil">
-                        <i class="fas fa-edit" id="${this.pencilId}"></i>
-                    </div>
-                    <p id="${this.paragraphId}" class="crossbow" style="margin-left: -3rem; margin-top: 1rem; width: 10px;">&times</p>
-                    <input type="checkbox" id="${this.checkBoxId}" style="width: 25%; height: 25%; margin-top: -7%; margin-left: -10rem;">
-                </div>
-            </div>
-        `
-    }
-
-    getInnerHtmlModal() {
-        return `
-            <div style="display: flex" id="${this.mainId}">
-                <div style="width: 45%" id="${this.divId}">
+            <div style="width: 100%; display: flex;" id="${this.mainId}">
+                <div style="width: 30%; padding-left: 2rem;" id="${this.divId}">
                     <p>
                         <strong id=${this.taskTextId}>Task: ${this.text}</strong> <br> 
                         <strong id=${this.creationDateId}>Creation Date: ${this.creationDate}</strong> <br>
                         <strong id=${this.expirationDateId}>Expiration Date: ${this.expirationDate}</strong> <br>
                     </p>
                 </div>
-                <div style="width: 45%;">
-                    <div class="pencil">
-                        <i class="fas fa-edit" id="${this.pencilId}"></i>
+                <div style="width: 45%; display: flex; flex-direction: column">
+                    <div style="display: flex; padding-top: 1rem; height: 50px">
+                        <i class="fas fa-edit" id="${this.pencilId}" style="padding-top: 0.7rem"></i>
+                        <p id="${this.paragraphId}" class="crossbow" style=" padding-left: 1rem">&times</p>
                     </div>
-                    <p id="${this.paragraphId}" class="crossbow" style="margin-left: -3rem; margin-top: 1rem; width: 10px;">&times</p>
-                    <input type="checkbox" id="${this.checkBoxId}" style="width: 25%; height: 25%; margin-top: -7%; margin-left: -10rem;">
+                    <div style="width: 100%; height: 100%;">
+                        <input type="checkbox" id="${this.checkBoxId}" style="width: 35%; height: 35%">
+                    </div>
                 </div>
             </div>
         `
@@ -78,4 +53,9 @@ export class Task {
         tasksObj.splice(removeElemIndex, 1);
     }
 
+    static replaceTaskWithChanged(changedObject) {
+        const removeElemIndex = tasksObj.findIndex(item => item.mainId === changedObject.mainId);
+
+        tasksObj.splice(removeElemIndex, 1, changedObject);
+    }
 }
